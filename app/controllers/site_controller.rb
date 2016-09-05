6aaa8057bec5a22858a4be492531b2b844365463
Jitang Zheng <jitang.zheng@gmail.com>
2016-09-05 15:48:44 -0700

    seo setup

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,9 +1,10 @@
 class SiteController < ApplicationController
+  before_action :must_admin!
 
   def info
     case request.method_symbol
       when :post
-        [:title, :subTitle, :copyright, :keywords, :description].each {|k| Setting.set_site_info k, params[k]}
+        [:title, :subTitle, :copyright, :keywords, :description].each { |k| Setting.set_site_info k, params[k] }
         Setting.site_author = params[:author]
         flash[:notice] = ' '
       else
@@ -12,7 +13,13 @@ class SiteController < ApplicationController
   end
 
   def seo
-
+    case request.method_symbol
+      when :post
+        [:google_site_id, :baidu_site_id].each { |k| Setting[k]= params[k] }
+        flash[:notice] = ' '
+      else
+        @info= Setting.get_site_info :title
+    end
   end
 
 
