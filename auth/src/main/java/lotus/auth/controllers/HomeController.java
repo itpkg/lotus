c8a74665851bf4490855fa94d2e0f88c1040f5e7
Jitang Zheng <jitang.zheng@gmail.com>
2016-09-19 06:57:46 -0700

    message source

--- auth/src/main/java/lotus/auth/controllers/HomeController.java
+++ auth/src/main/java/lotus/auth/controllers/HomeController.java
@@ -1,10 +1,16 @@
 package lotus.auth.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import java.util.Locale;
 
 /**
  * Created by flamen on 16-9-18.
@@ -12,7 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
     @RequestMapping(value = "/install", method = RequestMethod.GET)
-    public String getInstall( Model model) {
+    public String getInstall(Locale locale,  Model model) {
+        logger.debug("### "+locale.toLanguageTag()+" "+messageSource.getMessage("buttons.submit", null, locale));
         model.addAttribute("name", "haha");
         return "install";
     }
@@ -22,4 +29,9 @@ public class HomeController {
         model.addAttribute("name", "haha");
         return "home";
     }
+
+    @Resource
+    private MessageSource messageSource;
+    private final static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 }
