0d4cc95356249e8f4737cdd72638909114d7cc57
Jitang Zheng <jitang.zheng@gmail.com>
2016-09-21 06:39:49 -0700

    switch languages

---
+++ TOTO-LIST.md
@@ -0,0 +1,14 @@
+工作列表
+---
+
+* sitemap
+* rss.atom
+* google/baidu seo
+* devise mailer in production
+* sidekiq
+* site info
+* reading
+* email system
+* forum
+* shop
+* search
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,22 @@
 # RailsSettings Model
 class Setting < RailsSettings::Base
-  source Rails.root.join("config/app.yml")
+  source Rails.root.join('config/app.yml')
   namespace Rails.env
+
+  def self.get_site_info(key)
+    Setting["#{I18n.locale}://site//#{key}"] || "site.#{key}"
+  end
+
+  def self.set_site_info(key, val)
+    Setting["#{I18n.locale}://site//#{key}"] =val
+  end
+
+  def self.get_page(key)
+    Setting["#{I18n.locale}://page//#{key}"] || "site.#{key}"
+  end
+
+  def self.set_page(key, val)
+    Setting["#{I18n.locale}://page//#{key}"] =val
+  end
+
 end
