ae0d75f4a57140c93799192bcd752c4ae5f93319
Jitang Zheng <jitang.zheng@gmail.com>
2016-09-21 11:36:09 -0700

    jobs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,4 +6,22 @@ class DashboardController < ApplicationController
     Rails.cache.clear
     redirect_to dashboard_status_path
   end
+
+  def status
+    config   = Rails.configuration.database_configuration
+    case ActiveRecord::Base.connection.adapter_name
+      when 'PostgreSQL'
+        @database = [
+            'select version()',
+            "SELECT pg_size_pretty(pg_database_size('#{config[Rails.env]['database']}')) as size",
+            'SELECT count(*) as pool FROM pg_stat_activity WHERE NOT pid=pg_backend_pid()'
+
+        ].map do |sql|
+          rst = ActiveRecord::Base.connection.execute(sql)
+          rst.first
+        end
+      else
+        @database = ''
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,16 @@ Rails.application.routes.draw do
   resource :notices
 
   get 'dashboard' => 'dashboard#index'
-  %w(logs status users).each {|act| get "dashboard/#{act}"}
+  %w(logs status users).each { |act| get "dashboard/#{act}" }
   delete 'dashboard/cache'
 
   root 'home#index'
 
   devise_for :users
+
+  require 'sidekiq/web'
+  authenticate :user, lambda { |u| u.is_admin? } do
+    mount Sidekiq::Web => '/jobs'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
