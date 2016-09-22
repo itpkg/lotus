ac69299d43a009254b833b08444bb9e4c408debe
Jitang Zheng <jitang.zheng@gmail.com>
2016-09-21 20:03:58 -0700

    fix flash_box bugs

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,19 +4,12 @@ class Setting < RailsSettings::Base
   namespace Rails.env
 
   def self.get_site_info(key)
-    Setting["#{I18n.locale}://site//#{key}"] || "site.#{key}"
+    Setting["#{I18n.locale}://site//#{key}"] || nil
   end
 
   def self.set_site_info(key, val)
-    Setting["#{I18n.locale}://site//#{key}"] =val
+    Setting["#{I18n.locale}://site//#{key}"] = val
   end
 
-  def self.get_page(key)
-    Setting["#{I18n.locale}://page//#{key}"] || "site.#{key}"
-  end
-
-  def self.set_page(key, val)
-    Setting["#{I18n.locale}://page//#{key}"] =val
-  end
 
 end
