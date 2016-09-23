2df18c04401b38365c3c4e8a0dc46d0279cf2376
Jitang Zheng <jitang.zheng@gmail.com>
2016-09-23 09:47:06 -0700

    clean code for using gin

--- engines/blog/models.go
+++
@@ -1,9 +0,0 @@
-package blog
-
-//Site site info
-type Site struct {
-	Title     string
-	SubTitle  string
-	Lang      string
-	Copyright string
-}
--- engines/blog/mount.go
+++
@@ -1,8 +0,0 @@
-package blog
-
-import "github.com/urfave/cli"
-
-//Shell shell commands
-func (p *Engine) Shell() []cli.Command {
-	return []cli.Command{}
-}
