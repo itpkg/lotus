74e51ac3c91290320f498f2196df54466bdb3a2d
Jitang Zheng <jitang.zheng@gmail.com>
2016-09-23 13:32:24 -0700

    dao

--- engines/auth/engine.go
+++ engines/auth/engine.go
@@ -9,6 +9,8 @@ import (
 
 type Engine struct {
 	Cache web.Cache `inject:""`
+	Db    *gorm.DB  `inject:""`
+	Dao   *Dao      `inject:""`
 }
 
 func (p *Engine) Map(*inject.Graph) error {
@@ -20,7 +22,16 @@ func (p *Engine) Mount(*gin.Engine) {
 
 }
 
-func (p *Engine) Migrate(*gorm.DB) {}
+func (p *Engine) Migrate(db *gorm.DB) {
+	db.AutoMigrate(
+		&Setting{}, &Notice{}, &LeaveWord{},
+		&User{}, &Role{}, &Permission{}, &Log{},
+	)
+
+	db.Model(&User{}).AddUniqueIndex("idx_user_provider_type_id", "provider_type", "provider_id")
+	db.Model(&Role{}).AddUniqueIndex("idx_roles_name_resource_type_id", "name", "resource_type", "resource_id")
+	db.Model(&Permission{}).AddUniqueIndex("idx_permissions_user_role", "user_id", "role_id")
+}
 
 func (p *Engine) Seed() {}
 
