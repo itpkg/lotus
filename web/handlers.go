348503d7de19ba87611ae6b295f8c71bf81dc431
Jitang Zheng <jitang.zheng@gmail.com>
2016-09-23 11:19:04 -0700

    LocaleHandler

--- web/handlers.go
+++ web/handlers.go
@@ -1,1 +1,35 @@
 package web
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/text/language"
+)
+
+func LocaleHandler(c *gin.Context) {
+	// 1. Check URL arguments.
+	lng := c.Request.URL.Query().Get("locale")
+
+	// 2. Get language information from cookies.
+	if len(lng) == 0 {
+		if ck, er := c.Request.Cookie("locale"); er == nil {
+			lng = ck.Value
+		}
+	}
+
+	// 3. Get language information from 'Accept-Language'.
+	if len(lng) == 0 {
+		al := c.Request.Header.Get("Accept-Language")
+		if len(al) > 4 {
+			lng = al[:5]
+		}
+	}
+
+	matcher := language.NewMatcher([]language.Tag{
+		language.AmericanEnglish,
+		language.SimplifiedChinese,
+		language.TraditionalChinese,
+	})
+	tag, _, _ := matcher.Match(language.Make(lng))
+
+	c.Set("locale", &tag)
+}
